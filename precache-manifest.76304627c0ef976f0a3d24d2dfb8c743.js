a2761c5bfdf4f7985b399f9b2f6659e228710782
Nam Dang <sote1412@gmail.com>
2020-10-05 14:14:42 +0700

    Updates

---
+++ precache-manifest.76304627c0ef976f0a3d24d2dfb8c743.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cd04b99d166a46dfd2b1e2a0f50d3d9",
+    "url": "/learn-reactjs/exercise-2/index.html"
+  },
+  {
+    "revision": "2e1ce345ed727d76a42c",
+    "url": "/learn-reactjs/exercise-2/static/css/2.cfa7112b.chunk.css"
+  },
+  {
+    "revision": "0635bc4713531b498c72",
+    "url": "/learn-reactjs/exercise-2/static/css/main.0bde720a.chunk.css"
+  },
+  {
+    "revision": "2e1ce345ed727d76a42c",
+    "url": "/learn-reactjs/exercise-2/static/js/2.ae13fe78.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/learn-reactjs/exercise-2/static/js/2.ae13fe78.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0635bc4713531b498c72",
+    "url": "/learn-reactjs/exercise-2/static/js/main.41c49564.chunk.js"
+  },
+  {
+    "revision": "73a63d824185f84c2316",
+    "url": "/learn-reactjs/exercise-2/static/js/runtime-main.a30ddde2.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/learn-reactjs/exercise-2/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/learn-reactjs/exercise-2/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/learn-reactjs/exercise-2/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/learn-reactjs/exercise-2/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/learn-reactjs/exercise-2/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
